d4ab5f88225006d31b70bd8101f45facfaf449d0
Anna Vynnyk <78808060+vynnyka@users.noreply.github.com>
2023-03-08 01:11:40 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>    
+<!-- the header section -->
+<head>
+    <title>Our Project Website</title>
+    <link rel="stylesheet" type="text/css"
+          href="<?php echo $app_path ?>main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>Our Project Website</h1>
+</header>
+<main>
